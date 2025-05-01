af13d659b081308381ad6e8c94c872f40d88b20a
Dhananjay Jaybhaye <dhananjay.vilas.jaybhaye@gmail.com>
2025-05-01 19:47:06 +0530

    Add files via upload

---
+++ ngIf/app/app.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  public flag = true
+
+  public veg = true
+
+  public FoodOption = true // true means veg and false means non veg
+}
