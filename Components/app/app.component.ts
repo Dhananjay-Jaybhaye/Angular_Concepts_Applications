0d7739f40e8735246e9dce8359a471d3e4f0c220
Dhananjay Jaybhaye <dhananjay.vilas.jaybhaye@gmail.com>
2025-05-01 19:32:59 +0530

    Add files via upload

---
+++ Components/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DemoComponent } from './demo/demo.component';
+import { HelloComponent } from './hello/hello.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,DemoComponent,HelloComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Components';
+}
