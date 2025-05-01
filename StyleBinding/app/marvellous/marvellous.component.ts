b703cabc93434cbd9ac556daa656325807f6b891
Dhananjay Jaybhaye <dhananjay.vilas.jaybhaye@gmail.com>
2025-05-01 19:51:06 +0530

    Add files via upload

---
+++ StyleBinding/app/marvellous/marvellous.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marvellous',
+  imports: [],
+  templateUrl: './marvellous.component.html',
+  styleUrl: './marvellous.component.css'
+})
+export class MarvellousComponent {
+  public MyColour = "Orange"
+  public FoodPreference = true
+
+}
