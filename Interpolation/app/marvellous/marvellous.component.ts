b0c02d68b70d473c850d0dac3bf00f5254611913
Dhananjay Jaybhaye <dhananjay.vilas.jaybhaye@gmail.com>
2025-05-01 19:41:42 +0530

    Add files via upload

---
+++ Interpolation/app/marvellous/marvellous.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marvellous',
+  imports: [],
+  templateUrl: './marvellous.component.html',
+  styleUrl: './marvellous.component.css'
+})
+export class MarvellousComponent {
+
+  public BatchName = "Angular"
+
+  DemoFun()
+  {
+    return "Inside DemoFun() Function"
+  }
+
+  Display()
+  {
+    console.log("Inside Display")
+  }
+}
