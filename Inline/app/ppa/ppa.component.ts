6ea4587f32ea9ee107122aaf9407357ff829885d
Dhananjay Jaybhaye <dhananjay.vilas.jaybhaye@gmail.com>
2025-05-01 19:41:00 +0530

    Add files via upload

---
+++ Inline/app/ppa/ppa.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ppa',
+  imports: [],
+  template: `
+    <p>
+      ppa works!
+    </p>
+  `,
+  styles: ``
+})
+export class PPAComponent {
+
+}
---
+++ Inline/app/python/python.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-python',
+  imports: [],
+  template: `
+    <p>
+      python works!
+    </p>
+  `,
+  styleUrl: './python.component.css'
+})
+export class PythonComponent {
+
+}
