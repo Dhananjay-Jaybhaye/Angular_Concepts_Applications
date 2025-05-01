3738d0fe0d82fd4ef159e1ac96623a81c36c3e73
Dhananjay Jaybhaye <dhananjay.vilas.jaybhaye@gmail.com>
2025-05-01 19:53:24 +0530

    Add files via upload

---
+++ WithoutService/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BatchDetailsComponent } from './batch-details/batch-details.component';
+import { BatchListComponent } from './batch-list/batch-list.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,BatchDetailsComponent,BatchListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'WithoutService';
+}
